9664345512c5f70322a263e3765088d28d0ad384
pytherik <erikberndt@gmx.net>
2023-09-19 17:33:54 +0200

    typescript excurse creating data.ts

---
+++ src/data.ts
@@ -0,0 +1,30 @@
+// enum declares a set of named constants
+enum ReportType {
+  INCOME = 'income',
+  EXPENSE = 'expense',
+}
+
+// interface creates new custom datatype Data
+interface Data {
+  report: {
+    id: string;
+    source: string;
+    amount: number;
+    created_at: Date;
+    updated_at: Date;
+    type: ReportType;
+  }[];
+}
+
+export const data: Data = {
+  report: [],
+};
+
+data.report.push({
+  id: 'uuid',
+  source: 'Salary',
+  amount: 7500,
+  created_at: new Date(),
+  updated_at: new Date(),
+  type: ReportType.EXPENSE,
+});
